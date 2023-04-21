5617458ac65d39dd5f96099a6085af70e058ba1d
Leonardo Salazar <leonardosalo11@gmail.com>
2023-04-21 16:06:13 -0700

    nuevo archivo de inicio de sesion en la carpeta de home y se ha limpiado el index

--- SistemaArely/Sistema/modulos/home/views.py
+++ SistemaArely/Sistema/modulos/home/views.py
@@ -1,7 +1,22 @@
-from flask import render_template, Blueprint
+
+from flask import render_template, Blueprint, request
 
 bp_home = Blueprint("home", __name__) #No debe tener el mismo nombre que la ruta Saludar
 
 @bp_home.route('/')
 def home():
     return render_template("home/index.html")
+
+@bp_home.route('/inicio/')
+def inicio():
+    return render_template("home/inicio.html")
+
+@bp_home.route('/sesion/', methods =['GET','POST'])
+def sesion():
+    return render_template("home/index.html")
+    #usuario = request.form.get("usuario")
+    #contra = request.form.get("contra")
+    #print(usuario)
+    #print(contra)
+
+    #if (usuario == 'admin' & contra == '123'):
